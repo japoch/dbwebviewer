298c73b963e817e9b651f97b824992ff910b1623
Jan Posvic <jan.posvic@intenta-automotive.de>
2024-02-01 14:26:19 +0100

    GH-2 add backend server

---
+++ src/backend/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// initialize Sequelize
+const db = require("./app/models");
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+//db.sequelize.sync({ force: true }).then(() => {
+//  console.log("Drop and re-sync db.");
+//});
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Hi!" });
+});
+
+require("./app/routes/tutorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8082;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
